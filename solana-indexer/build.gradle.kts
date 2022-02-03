5c62d637ecd8c4dd825e13c43864b14808b6df8a
Mikhail Nasimov <m.nasimov@rarible.com>
2022-02-03 14:35:23 +0300

    add core apm starter

--- solana-indexer/build.gradle.kts
+++ solana-indexer/build.gradle.kts
@@ -1,9 +1,12 @@
 apply(plugin = "server")
 
 val solanaOpenapiVersion: String by project
+val raribleCommonVersion: String by project
 
 dependencies {
     implementation(project(":solana-common"))
     implementation(project(":util-borsh"))
+    implementation("com.rarible.core:rarible-core-apm-starter:$raribleCommonVersion")
+
     testImplementation("com.rarible.solana.protocol:solana-protocol-subscriber-starter:$solanaOpenapiVersion")
 }
