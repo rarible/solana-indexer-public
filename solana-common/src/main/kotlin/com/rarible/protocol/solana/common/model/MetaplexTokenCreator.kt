003fba8e9825055936c91dfa3d6acffb3fc67b15
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-03-03 14:10:55 +0100

    fix: restore binary compatibility.

--- solana-common/src/main/kotlin/com/rarible/protocol/solana/common/model/MetaplexTokenCreator.kt
+++ solana-common/src/main/kotlin/com/rarible/protocol/solana/common/model/MetaplexTokenCreator.kt
@@ -3,5 +3,7 @@ package com.rarible.protocol.solana.common.model
 data class MetaplexTokenCreator(
     val address: String,
     val share: Int,
-    val verified: Boolean
+    // TODO[compat]: 'null' only to be compatible with previous messages.
+    //  Make not null before deploying.
+    val verified: Boolean? = null
 )
