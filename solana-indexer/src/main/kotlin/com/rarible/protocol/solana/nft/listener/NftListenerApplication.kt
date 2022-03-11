0772accc8956687204f3b942d2f6ebb46d2f4d1e
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-03-11 12:28:40 +0100

    feat: add config to run locally.

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/NftListenerApplication.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/NftListenerApplication.kt
@@ -10,5 +10,19 @@ import org.springframework.context.annotation.Import
 class NftListenerApplication
 
 fun main(args: Array<String>) {
+    if (System.getProperty("LOCAL") == "true") {
+        setPropertiesToRunLocally()
+    }
     runApplication<NftListenerApplication>(*args)
+}
+
+// Note! Also, to run locally you should start MongoDB & Kafka containers locally
+// and tune parameters in the 'application-local.yml' (PLAINTEXT://localhost:53502, mongodb://localhost:53511)
+private fun setPropertiesToRunLocally() {
+    System.setProperty("spring.profiles.active", "local")
+    System.setProperty("spring.cloud.service-registry.auto-registration.enabled", "false")
+    System.setProperty("spring.cloud.discovery.enabled", "false")
+    System.setProperty("spring.cloud.consul.config.enabled", "false")
+    System.setProperty("logging.logstash.tcp-socket.enabled", "false")
+    System.setProperty("application.environment", "local")
 }
