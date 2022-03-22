70a133f043d686aafdd566a15fd0bf4160a036ad
Artem Yakischik <a.yakischik@rarible.com>
2022-03-22 18:34:31 +0400

    cleanup

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/service/subscribers/filter/SolanaTokenFilter.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/service/subscribers/filter/SolanaTokenFilter.kt
@@ -2,10 +2,6 @@ package com.rarible.protocol.solana.nft.listener.service.subscribers.filter
 
 interface SolanaTokenFilter {
 
-    companion object {
-
-    }
-
     fun isAcceptableToken(mint: String): Boolean
 
 }
