865703b4643761bc46692b914a6704595ff8b94b
Eugene Nacu <evgeny.nacu@gmail.com>
2022-03-24 11:16:51 +0000

    change prefix for block cache properties

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/BlockCacheProperties.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/BlockCacheProperties.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "solana.block.cache")
+@ConfigurationProperties(prefix = "block.cache")
 data class BlockCacheProperties(
     val enabled: Boolean = false,
     val rpcApiUrls: List<String> = listOf(),
