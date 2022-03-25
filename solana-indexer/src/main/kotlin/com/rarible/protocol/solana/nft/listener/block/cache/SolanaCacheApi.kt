12b46dce7817df7f5a2c539f5052bc4bb369a30b
Eugene Nacu <evgeny.nacu@gmail.com>
2022-03-25 22:16:05 +0000

    fix: save block to cache + log it

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/SolanaCacheApi.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/SolanaCacheApi.kt
@@ -11,6 +11,8 @@ import com.rarible.blockchain.scanner.solana.client.dto.SolanaTransactionDto
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class SolanaCacheApi(
     private val repository: BlockCacheRepository,
@@ -39,24 +41,24 @@ class SolanaCacheApi(
         slot: Long,
         details: GetBlockRequest.TransactionDetails
     ): ApiResponse<SolanaBlockDto> {
-        val fetchStart = Timer.start()
-        val fromCache = repository.find(slot)
-        fetchStart.stop(blockCacheFetchTimer)
-        if (fromCache != null) {
-            blockCacheHits.increment()
-            blockCacheLoadedSize.increment(fromCache.size.toDouble())
+        return if (details == GetBlockRequest.TransactionDetails.None) {
+            httpApi.getBlock(slot, details)
         } else {
-            blockCacheMisses.increment()
-        }
-        val bytes = fromCache ?: return httpApi.getBlock(slot, details)
-        val apiResponse = mapper.readValue<ApiResponse<SolanaBlockDto>>(bytes)
-
-        return when (details) {
-            GetBlockRequest.TransactionDetails.Full -> apiResponse
-            GetBlockRequest.TransactionDetails.None -> apiResponse.copy(
-                error = apiResponse.error,
-                result = apiResponse.result?.copy(transactions = emptyList())
-            )
+            val fetchStart = Timer.start()
+            val fromCache = repository.find(slot)
+            fetchStart.stop(blockCacheFetchTimer)
+            if (fromCache != null) {
+                blockCacheHits.increment()
+                blockCacheLoadedSize.increment(fromCache.size.toDouble())
+                mapper.readValue(fromCache)
+            } else {
+                blockCacheMisses.increment()
+                val result = httpApi.getBlock(slot, details)
+                val bytes = mapper.writeValueAsBytes(result)
+                repository.save(slot, bytes)
+                logger.info("Saved block to cache: {} size: {}", slot, bytes.size)
+                result
+            }
         }
     }
 
@@ -67,6 +69,7 @@ class SolanaCacheApi(
     override suspend fun getTransaction(signature: String): ApiResponse<SolanaTransactionDto> = httpApi.getTransaction(signature)
 
     private companion object {
+        val logger: Logger = LoggerFactory.getLogger(SolanaCacheApi::class.java)
         const val BLOCK_CACHE_TIMER = "block_cache_fetch_timer"
         const val BLOCK_CACHE_LOADED_SIZE = "block_cache_loaded_size"
         const val BLOCK_CACHE_HITS = "block_cache_hits"
