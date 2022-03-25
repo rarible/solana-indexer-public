849e979f35a3c32239ef92a1eb5d1182c4519087
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-03-26 00:21:19 +0300

    feat: add metrics for cache hits and misses.

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/SolanaCacheApi.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/SolanaCacheApi.kt
@@ -8,17 +8,47 @@ import com.rarible.blockchain.scanner.solana.client.dto.ApiResponse
 import com.rarible.blockchain.scanner.solana.client.dto.GetBlockRequest
 import com.rarible.blockchain.scanner.solana.client.dto.SolanaBlockDto
 import com.rarible.blockchain.scanner.solana.client.dto.SolanaTransactionDto
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 
 class SolanaCacheApi(
-    val repository: BlockCacheRepository,
-    val httpApi: SolanaHttpRpcApi,
-    val mapper: ObjectMapper
+    private val repository: BlockCacheRepository,
+    private val httpApi: SolanaHttpRpcApi,
+    private val mapper: ObjectMapper,
+    meterRegistry: MeterRegistry
 ) : SolanaApi {
+
+    private val blockCacheFetchTimer = Timer
+        .builder(BLOCK_CACHE_TIMER)
+        .register(meterRegistry)
+
+    private val blockCacheLoadedSize = Counter
+        .builder(BLOCK_CACHE_LOADED_SIZE)
+        .register(meterRegistry)
+
+    private val blockCacheHits = Counter
+        .builder(BLOCK_CACHE_HITS)
+        .register(meterRegistry)
+
+    private val blockCacheMisses = Counter
+        .builder(BLOCK_CACHE_MISSES)
+        .register(meterRegistry)
+
     override suspend fun getBlock(
         slot: Long,
         details: GetBlockRequest.TransactionDetails
     ): ApiResponse<SolanaBlockDto> {
-        val bytes = repository.find(slot) ?: return httpApi.getBlock(slot, details)
+        val fetchStart = Timer.start()
+        val fromCache = repository.find(slot)
+        fetchStart.stop(blockCacheFetchTimer)
+        if (fromCache != null) {
+            blockCacheHits.increment()
+            blockCacheLoadedSize.increment(fromCache.size.toDouble())
+        } else {
+            blockCacheMisses.increment()
+        }
+        val bytes = fromCache ?: return httpApi.getBlock(slot, details)
         val apiResponse = mapper.readValue<ApiResponse<SolanaBlockDto>>(bytes)
 
         return when (details) {
@@ -35,4 +65,11 @@ class SolanaCacheApi(
     override suspend fun getLatestSlot(): ApiResponse<Long> = httpApi.getLatestSlot()
 
     override suspend fun getTransaction(signature: String): ApiResponse<SolanaTransactionDto> = httpApi.getTransaction(signature)
+
+    private companion object {
+        const val BLOCK_CACHE_TIMER = "block_cache_fetch_timer"
+        const val BLOCK_CACHE_LOADED_SIZE = "block_cache_loaded_size"
+        const val BLOCK_CACHE_HITS = "block_cache_hits"
+        const val BLOCK_CACHE_MISSES = "block_cache_misses"
+    }
 }
