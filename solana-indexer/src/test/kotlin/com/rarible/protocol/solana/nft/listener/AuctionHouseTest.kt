38e9d8e88bf731ac7fde1205564c41b1f2870664
Mikhail Nasimov <m.nasimov@rarible.com>
2022-03-28 16:53:35 +0300

    Amend sale test

--- solana-indexer/src/test/kotlin/com/rarible/protocol/solana/nft/listener/AuctionHouseTest.kt
+++ solana-indexer/src/test/kotlin/com/rarible/protocol/solana/nft/listener/AuctionHouseTest.kt
@@ -6,14 +6,14 @@ import com.rarible.core.test.wait.Wait
 import com.rarible.protocol.solana.common.model.WrappedSolAssetType
 import com.rarible.protocol.solana.common.records.SolanaAuctionHouseOrderRecord
 import com.rarible.protocol.solana.common.records.SolanaAuctionHouseRecord
+import com.rarible.protocol.solana.common.records.SolanaBalanceRecord
 import com.rarible.protocol.solana.nft.listener.service.subscribers.SubscriberGroup
 import com.rarible.protocol.solana.test.ANY_SOLANA_LOG
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Disabled
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
@@ -23,7 +23,6 @@ import org.springframework.data.mongodb.core.query.isEqualTo
 import java.time.Duration
 import java.time.Instant
 
-@Disabled // TODO too slow or even endless
 class AuctionHouseTest : AbstractBlockScannerTest() {
 
     private val timeout = Duration.ofSeconds(5)
@@ -45,7 +44,7 @@ class AuctionHouseTest : AbstractBlockScannerTest() {
                 type = SolanaAuctionHouseRecord.CreateAuctionHouseRecord::class.java
             ).toList()
 
-            Assertions.assertThat(records).usingElementComparatorIgnoringFields(
+            assertThat(records).usingElementComparatorIgnoringFields(
                 SolanaAuctionHouseRecord.CreateAuctionHouseRecord::log.name,
                 SolanaAuctionHouseRecord.CreateAuctionHouseRecord::timestamp.name
             ).isEqualTo(
@@ -80,7 +79,7 @@ class AuctionHouseTest : AbstractBlockScannerTest() {
                 type = SolanaAuctionHouseOrderRecord.SellRecord::class.java
             ).toList()
 
-            Assertions.assertThat(records).usingElementComparatorIgnoringFields(
+            assertThat(records).usingElementComparatorIgnoringFields(
                 SolanaAuctionHouseOrderRecord.SellRecord::log.name,
                 SolanaAuctionHouseOrderRecord.SellRecord::timestamp.name,
                 SolanaAuctionHouseOrderRecord.SellRecord::tokenAccount.name // TODO: do not ignore. We can calculate this field by [token] and [baseKeyPair]
@@ -117,7 +116,7 @@ class AuctionHouseTest : AbstractBlockScannerTest() {
                 type = SolanaAuctionHouseOrderRecord.BuyRecord::class.java
             ).toList()
 
-            Assertions.assertThat(records).usingElementComparatorIgnoringFields(
+            assertThat(records).usingElementComparatorIgnoringFields(
                 SolanaAuctionHouseOrderRecord.BuyRecord::log.name,
                 SolanaAuctionHouseOrderRecord.BuyRecord::timestamp.name,
                 SolanaAuctionHouseOrderRecord.BuyRecord::tokenAccount.name // TODO: do not ignore. We can calculate this field by [token] and [baseKeyPair]
@@ -157,13 +156,61 @@ class AuctionHouseTest : AbstractBlockScannerTest() {
         buy(house, buyerKeypair, 1, token, 1)
 
         executeSale(house, auctionHouseKeypair, 1, token, 1, buyerWallet = buyerWallet, sellerWallet = sellerWallet)
-
         Wait.waitAssert(timeout) {
-            val records = findRecordByType(
+            val balanceRecords  = findRecordByType(
+                collection = SubscriberGroup.BALANCE.collectionName,
+                type = SolanaBalanceRecord.InitializeBalanceAccountRecord::class.java
+            ).toList()
+            val fromAccount = balanceRecords.single { it.owner == sellerWallet }.balanceAccount
+            val toAccount = balanceRecords.single { it.owner == buyerWallet }.balanceAccount
+            val buyRecords = findRecordByType(
+                collection = SubscriberGroup.AUCTION_HOUSE_ORDER.collectionName,
+                type = SolanaAuctionHouseOrderRecord.BuyRecord::class.java
+            ).toList()
+            assertThat(buyRecords).usingElementComparatorIgnoringFields(
+                SolanaAuctionHouseOrderRecord.BuyRecord::log.name,
+                SolanaAuctionHouseOrderRecord.BuyRecord::timestamp.name
+            ).isEqualTo(
+                listOf(
+                    SolanaAuctionHouseOrderRecord.BuyRecord(
+                        maker = buyerWallet,
+                        buyPrice = 1.scaleSupply(9),
+                        tokenAccount = fromAccount,
+                        mint = token,
+                        amount = 1L.toBigInteger(),
+                        auctionHouse = house,
+                        log = ANY_SOLANA_LOG,
+                        timestamp = Instant.EPOCH,
+                    )
+                )
+            )
+
+            val sellRecords = findRecordByType(
+                collection = SubscriberGroup.AUCTION_HOUSE_ORDER.collectionName,
+                type = SolanaAuctionHouseOrderRecord.SellRecord::class.java
+            ).toList()
+            assertThat(sellRecords).usingElementComparatorIgnoringFields(
+                SolanaAuctionHouseOrderRecord.SellRecord::log.name,
+                SolanaAuctionHouseOrderRecord.SellRecord::timestamp.name
+            ).isEqualTo(
+                listOf(
+                    SolanaAuctionHouseOrderRecord.SellRecord(
+                        maker = sellerWallet,
+                        sellPrice = 1.scaleSupply(9),
+                        tokenAccount = fromAccount,
+                        mint = token,
+                        amount = 1L.toBigInteger(),
+                        auctionHouse = house,
+                        log = ANY_SOLANA_LOG,
+                        timestamp = Instant.EPOCH,
+                    )
+                )
+            )
+
+            val saleRecords = findRecordByType(
                 collection = SubscriberGroup.AUCTION_HOUSE_ORDER.collectionName,
                 type = SolanaAuctionHouseOrderRecord.ExecuteSaleRecord::class.java
             ).toList()
-
             val sellRecord = SolanaAuctionHouseOrderRecord.ExecuteSaleRecord(
                 buyer = buyerWallet,
                 seller = sellerWallet,
@@ -176,13 +223,53 @@ class AuctionHouseTest : AbstractBlockScannerTest() {
                 direction = SolanaAuctionHouseOrderRecord.ExecuteSaleRecord.Direction.SELL,
                 treasuryMint = wrappedSol
             )
-            Assertions.assertThat(records).usingElementComparatorIgnoringFields(
+            assertThat(saleRecords).usingElementComparatorIgnoringFields(
                 SolanaAuctionHouseOrderRecord.ExecuteSaleRecord::log.name,
                 SolanaAuctionHouseOrderRecord.ExecuteSaleRecord::timestamp.name
             ).isEqualTo(
                 listOf(
                     sellRecord,
                     sellRecord.copy(direction = SolanaAuctionHouseOrderRecord.ExecuteSaleRecord.Direction.BUY)
+                )
+            )
+
+            val incomeTransfersRecords  = findRecordByType(
+                collection = SubscriberGroup.BALANCE.collectionName,
+                type = SolanaBalanceRecord.TransferIncomeRecord::class.java
+            ).toList()
+            assertThat(incomeTransfersRecords).usingElementComparatorIgnoringFields(
+                SolanaBalanceRecord.TransferIncomeRecord::log.name,
+                SolanaBalanceRecord.TransferIncomeRecord::timestamp.name
+            ).isEqualTo(
+                listOf(
+                    SolanaBalanceRecord.TransferIncomeRecord(
+                        from = fromAccount,
+                        owner = toAccount,
+                        mint = token,
+                        incomeAmount = 1.toBigInteger(),
+                        log = ANY_SOLANA_LOG,
+                        timestamp = Instant.EPOCH,
+                    )
+                )
+            )
+
+            val outcomeTransfersRecords  = findRecordByType(
+                collection = SubscriberGroup.BALANCE.collectionName,
+                type = SolanaBalanceRecord.TransferOutcomeRecord::class.java
+            ).toList()
+            assertThat(outcomeTransfersRecords).usingElementComparatorIgnoringFields(
+                SolanaBalanceRecord.TransferOutcomeRecord::log.name,
+                SolanaBalanceRecord.TransferOutcomeRecord::timestamp.name
+            ).isEqualTo(
+                listOf(
+                    SolanaBalanceRecord.TransferOutcomeRecord(
+                        to = toAccount,
+                        owner = fromAccount,
+                        mint = token,
+                        outcomeAmount = 1.toBigInteger(),
+                        log = ANY_SOLANA_LOG,
+                        timestamp = Instant.EPOCH,
+                    )
                 )
             )
         }
