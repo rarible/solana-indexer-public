2d01b2ea7b2474cb8d0ef0fce3674defa3d94db3
Mikhail Nasimov <m.nasimov@rarible.com>
2022-04-04 17:18:46 +0300

    Add migration module

--- solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002ReworkBalanceRecords.kt
+++ solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002ReworkBalanceRecords.kt
@@ -1,12 +1,15 @@
 package com.rarible.protocol.solana.nft.migration.mongock
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
 import com.rarible.protocol.solana.common.records.SubscriberGroup
 import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.runBlocking
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 
 @ChangeLog(order = "00002")
@@ -16,31 +19,41 @@ class ChangeLog00002CreateIndices {
         id = "ChangeLog00002CreateIndices.reworkBalanceRecords",
         order = "00002",
         author = "protocol",
-        runAlways = false
+        runAlways = true
     )
     fun reworkBalanceRecords(
         mongoOperations: ReactiveMongoOperations
     ) = runBlocking {
+        val mapper = ObjectMapper()
+
         mongoOperations.find(Query(), ObjectNode::class.java, SubscriberGroup.BALANCE.collectionName)
             .asFlow()
             .collect {
-                val balanceAccount = it["balanceAccount"].asText()
+                val clazz = it["_class"].asText()
 
-                if (balanceAccount != null) {
-                    mongoOperations.remove(it)
-                    it.remove("balanceAccount")
-                    it.put("account", balanceAccount)
-                    mongoOperations.insert(it, SubscriberGroup.BALANCE.collectionName)
-                } else {
-                    val owner = it["owner"].asText()
-
-                    if (owner != null) {
-                        mongoOperations.remove(it)
-                        it.remove("owner")
-                        it.put("account", owner)
-                        mongoOperations.insert(it, SubscriberGroup.BALANCE.collectionName)
-                    }
+                when {
+                    "InitializeBalanceAccountRecord" in clazz -> it.replaceTextNode("balanceAccount", "account")
+                    "TransferIncomeRecord" in clazz -> it.replaceTextNode("owner", "account")
+                    "TransferOutcomeRecord" in clazz -> it.replaceTextNode("owner", "account")
+                    else -> return@collect
                 }
+
+                val criteria = Criteria.where("_id").`is`(it["_id"].textValue())
+
+                mongoOperations.remove(
+                    Query(criteria),
+                    SubscriberGroup.BALANCE.collectionName
+                ).awaitFirst()
+
+                mongoOperations.insert(mapper.writeValueAsString(it), SubscriberGroup.BALANCE.collectionName).awaitFirst()
             }
+    }
+
+    private fun ObjectNode.replaceTextNode(src: String, dst: String) {
+        val old = remove(src).textValue()
+
+        check(old.isNotBlank())
+
+        put(dst, old)
     }
 }
