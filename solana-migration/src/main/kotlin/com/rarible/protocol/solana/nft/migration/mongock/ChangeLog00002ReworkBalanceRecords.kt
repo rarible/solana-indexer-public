b9e7c1aca5ed4ddafbc012e503bbb5091acdb7e3
Mikhail Nasimov <m.nasimov@rarible.com>
2022-04-04 18:43:41 +0300

    Add migration module

--- solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002ReworkBalanceRecords.kt
+++ solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002ReworkBalanceRecords.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.runBlocking
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
@@ -27,27 +28,28 @@ class ChangeLog00002ReworkBalanceRecords {
     fun reworkBalanceRecords(
         @NonLockGuarded mongoOperations: ReactiveMongoOperations
     ) = runBlocking {
-        mongoOperations.find(Query(), ObjectNode::class.java, SubscriberGroup.BALANCE.collectionName)
-            .asFlow()
-            .withIndex()
-            .collect {
-                if (it.index % 10000 == 0) println("${it.index * 10000} balance entities processed")
-                val clazz = it.value["_class"].asText()
+        mongoOperations.find(
+            Query().with(Sort.by(Sort.Direction.ASC, "_id")),
+            ObjectNode::class.java,
+            SubscriberGroup.BALANCE.collectionName
+        ).asFlow().withIndex().collect {
+            if (it.index % 10000 == 0) println("${it.index} balance entities processed")
+            val clazz = it.value["_class"].asText()
 
-                val update = when {
-                    "InitializeBalanceAccountRecord" in clazz -> Update().rename("balanceAccount", "account")
-                    "TransferIncomeRecord" in clazz -> Update().rename("owner", "account")
-                    "TransferOutcomeRecord" in clazz -> Update().rename("owner", "account")
-                    else -> return@collect
-                }
-
-                val criteria = Criteria.where("_id").`is`(it.value["_id"].textValue())
-
-                mongoOperations.updateFirst(
-                    Query(criteria),
-                    update,
-                    SubscriberGroup.BALANCE.collectionName
-                ).awaitFirst()
+            val update = when {
+                "InitializeBalanceAccountRecord" in clazz -> Update().rename("balanceAccount", "account")
+                "TransferIncomeRecord" in clazz -> Update().rename("owner", "account")
+                "TransferOutcomeRecord" in clazz -> Update().rename("owner", "account")
+                else -> return@collect
             }
+
+            val criteria = Criteria.where("_id").`is`(it.value["_id"].textValue())
+
+            mongoOperations.updateFirst(
+                Query(criteria),
+                update,
+                SubscriberGroup.BALANCE.collectionName
+            ).awaitFirst()
+        }
     }
 }
