bad5da2bb013f3f772d8bb8848a7faaf45933ae7
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-04-05 23:43:04 +0300

    fix: save activities to the repository.

--- solana-common/src/main/kotlin/com/rarible/protocol/solana/common/update/ActivityEventListener.kt
+++ solana-common/src/main/kotlin/com/rarible/protocol/solana/common/update/ActivityEventListener.kt
@@ -1,15 +1,24 @@
 package com.rarible.protocol.solana.common.update
 
 import com.rarible.core.kafka.RaribleKafkaProducer
+import com.rarible.protocol.solana.common.repository.ActivityRepository
 import com.rarible.protocol.solana.dto.ActivityDto
 import org.springframework.stereotype.Component
 
 @Component
 class ActivityEventListener(
-    private val publisher: RaribleKafkaProducer<ActivityDto>
+    private val publisher: RaribleKafkaProducer<ActivityDto>,
+    private val activityRepository: ActivityRepository
 ) {
 
     suspend fun onActivities(activities: List<ActivityDto>) {
+        for (activity in activities) {
+            if (activity.reverted) {
+                activityRepository.removeById(activity.id)
+            } else {
+                activityRepository.save(activity)
+            }
+        }
         val messages = activities.map { KafkaEventFactory.activityEvent(it) }
         publisher.send(messages).collect { it.ensureSuccess() }
     }
