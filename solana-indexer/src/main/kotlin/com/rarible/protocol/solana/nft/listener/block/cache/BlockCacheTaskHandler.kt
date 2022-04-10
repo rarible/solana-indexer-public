fc72c8839de59fa2537f0f819008f025a3f1f508
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-04-10 09:16:01 +0300

    feat: add metrics for loading/saving batch of blocks.

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/BlockCacheTaskHandler.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/block/cache/BlockCacheTaskHandler.kt
@@ -3,6 +3,8 @@ package com.rarible.protocol.solana.nft.listener.block.cache
 import com.rarible.blockchain.scanner.solana.client.dto.GetBlockRequest
 import com.rarible.core.apm.withTransaction
 import com.rarible.core.task.TaskHandler
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
@@ -12,13 +14,20 @@ import kotlinx.coroutines.flow.map
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class BlockCacheTaskHandler(
     private val client: BlockCacheClient,
     private val repository: BlockCacheRepository,
     private val blockCacheProperties: BlockCacheProperties,
+    private val meterRegistry: MeterRegistry
 ) : TaskHandler<Long> {
+
+    private val blockCacheSaveTimer by lazy { meterRegistry.timer("block_cache_save") }
+
+    private val blockCacheLoadTimer by lazy { meterRegistry.timer("block_cache_load") }
+
     override val type: String = "BLOCK_CACHE"
 
     override fun runLongTask(from: Long?, param: String): Flow<Long> {
@@ -34,6 +43,7 @@ class BlockCacheTaskHandler(
 
     private suspend fun loadRange(blocks: LongRange) {
         coroutineScope {
+            val loadStartTime = Timer.start()
             val loadedBlocks = blocks.map {
                 async {
                     if (!repository.isPresent(it)) {
@@ -46,9 +56,15 @@ class BlockCacheTaskHandler(
                     }
                 }
             }.awaitAll().filterNotNull().toMap()
+            logger.info("Loaded ${blocks.count()} blocks in ${loadStartTime.stop(blockCacheLoadTimer).nanoToMillis()}")
+
+            val saveStartTime = Timer.start()
             repository.save(loadedBlocks)
+            logger.info("Saved ${blocks.count()} blocks in ${saveStartTime.stop(blockCacheSaveTimer).nanoToMillis()}")
         }
     }
+
+    private fun Long.nanoToMillis() = TimeUnit.NANOSECONDS.toMillis(this)
 
     companion object {
         val logger: Logger = LoggerFactory.getLogger(BlockCacheTaskHandler::class.java)
@@ -56,7 +72,7 @@ class BlockCacheTaskHandler(
 }
 
 private fun getRangesReversed(from: Long, to: Long, step: Int): Sequence<LongRange> {
-    check(from >= 0) { "$from "}
+    check(from >= 0) { "$from " }
     check(to >= 0) { "$to" }
     if (from > to) return emptySequence()
     return (from..to).reversed().asSequence().chunked(step) { list ->
