5b074353eb0028be562d209e8811d6840bd0b2f4
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-04-11 15:28:21 +0300

    fix: create Mongo indexes for Metaplex records.

--- solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00001CreateIndices.kt
+++ solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00001CreateIndices.kt
@@ -9,6 +9,7 @@ import com.rarible.protocol.solana.common.repository.MetaplexOffChainMetaReposit
 import com.rarible.protocol.solana.common.repository.OrderRepository
 import com.rarible.protocol.solana.common.repository.SolanaAuctionHouseOrderRecordsRepository
 import com.rarible.protocol.solana.common.repository.SolanaBalanceRecordsRepository
+import com.rarible.protocol.solana.common.repository.SolanaMetaplexMetaRecordsRepository
 import com.rarible.protocol.solana.common.repository.SolanaTokenRecordsRepository
 import com.rarible.protocol.solana.common.repository.TokenRepository
 import io.changock.migration.api.annotations.NonLockGuarded
@@ -33,7 +34,8 @@ class ChangeLog00001CreateIndices {
         @NonLockGuarded balanceRecordsRepository: SolanaBalanceRecordsRepository,
         @NonLockGuarded tokenRecordsRepository: SolanaTokenRecordsRepository,
         @NonLockGuarded orderRecordsRepository: SolanaAuctionHouseOrderRecordsRepository,
-    ) = runBlocking {
+        @NonLockGuarded metaRecordsRepository: SolanaMetaplexMetaRecordsRepository,
+        ) = runBlocking {
         balanceRepository.createIndexes()
         tokenRepository.createIndexes()
         metaplexMetaRepository.createIndexes()
@@ -43,5 +45,6 @@ class ChangeLog00001CreateIndices {
         tokenRecordsRepository.createIndexes()
         balanceRecordsRepository.createIndexes()
         orderRecordsRepository.createIndexes()
+        metaRecordsRepository.createIndexes()
     }
 }
