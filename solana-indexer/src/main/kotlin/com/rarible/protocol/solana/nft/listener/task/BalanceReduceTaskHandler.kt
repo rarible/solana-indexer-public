b47ef383dcdaad75d7d23fbbc9cd31a149666f1c
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-04-11 14:31:41 +0300

    fix: during full reduce save the activities.

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/task/BalanceReduceTaskHandler.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/task/BalanceReduceTaskHandler.kt
@@ -2,16 +2,19 @@ package com.rarible.protocol.solana.nft.listener.task
 
 import com.rarible.core.entity.reducer.service.StreamFullReduceService
 import com.rarible.core.task.TaskHandler
+import com.rarible.protocol.solana.common.converter.SolanaBalanceActivityConverter
 import com.rarible.protocol.solana.common.event.BalanceEvent
 import com.rarible.protocol.solana.common.model.Balance
 import com.rarible.protocol.solana.common.model.BalanceId
 import com.rarible.protocol.solana.common.records.SolanaBalanceRecord
+import com.rarible.protocol.solana.common.repository.ActivityRepository
 import com.rarible.protocol.solana.common.repository.SolanaBalanceRecordsRepository
 import com.rarible.protocol.solana.nft.listener.service.balance.BalanceEventConverter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort
@@ -24,7 +27,9 @@ typealias BalanceStreamFullReduceService = StreamFullReduceService<BalanceId, Ba
 class BalanceReduceTaskHandler(
     private val balanceStreamFullReduceService: BalanceStreamFullReduceService,
     private val balanceRecordsRepository: SolanaBalanceRecordsRepository,
-    private val balanceEventConverter: BalanceEventConverter
+    private val balanceEventConverter: BalanceEventConverter,
+    private val activityRepository: ActivityRepository,
+    private val balanceActivityConverter: SolanaBalanceActivityConverter
 ) : TaskHandler<String> {
     override val type: String = "BALANCE_REDUCER"
 
@@ -40,12 +45,20 @@ class BalanceReduceTaskHandler(
         val balanceFlow = balanceRecordsRepository.findBy(
             criteria,
             Sort.by(Sort.Direction.ASC, SolanaBalanceRecord::account.name, "_id"),
-        ).flatMapConcat {
+        ).onEach { saveBalanceActivity(it) }.flatMapConcat {
             balanceEventConverter.convert(it, false).asFlow()
         }
 
 
         return balanceStreamFullReduceService.reduce(balanceFlow).map { it.account }
+    }
+
+    // TODO: ideally, we should also remove all redundant left-over activities by this entity.
+    private suspend fun saveBalanceActivity(balanceRecord: SolanaBalanceRecord) {
+        val activityDto = balanceActivityConverter.convert(balanceRecord, false)
+        if (activityDto != null) {
+            activityRepository.save(activityDto)
+        }
     }
 
     companion object {
--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/task/OrderReduceTaskHandler.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/task/OrderReduceTaskHandler.kt
@@ -2,16 +2,19 @@ package com.rarible.protocol.solana.nft.listener.task
 
 import com.rarible.core.entity.reducer.service.StreamFullReduceService
 import com.rarible.core.task.TaskHandler
+import com.rarible.protocol.solana.common.converter.SolanaAuctionHouseOrderActivityConverter
 import com.rarible.protocol.solana.common.event.OrderEvent
 import com.rarible.protocol.solana.common.model.Order
 import com.rarible.protocol.solana.common.model.OrderId
 import com.rarible.protocol.solana.common.records.SolanaAuctionHouseOrderRecord
+import com.rarible.protocol.solana.common.repository.ActivityRepository
 import com.rarible.protocol.solana.common.repository.SolanaAuctionHouseOrderRecordsRepository
 import com.rarible.protocol.solana.nft.listener.service.order.OrderEventConverter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort
@@ -24,7 +27,9 @@ typealias OrderStreamFullReduceService = StreamFullReduceService<OrderId, OrderE
 class OrderReduceTaskHandler(
     private val orderStreamFullReduceService: OrderStreamFullReduceService,
     private val orderRecordsRepository: SolanaAuctionHouseOrderRecordsRepository,
-    private val orderEventConverter: OrderEventConverter
+    private val orderEventConverter: OrderEventConverter,
+    private val activityRepository: ActivityRepository,
+    private val orderActivityConverter: SolanaAuctionHouseOrderActivityConverter
 ) : TaskHandler<String> {
     override val type: String = "ORDER_REDUCER"
 
@@ -48,11 +53,18 @@ class OrderReduceTaskHandler(
                 SolanaAuctionHouseOrderRecord::orderId.name,
                 "_id",
             )
-        ).flatMapConcat {
+        ).onEach { saveOrderActivity(it) }.flatMapConcat {
             orderEventConverter.convert(it, false).asFlow()
         }
 
         return orderStreamFullReduceService.reduce(orderFlow).map { it.id }
+    }
+
+    private suspend fun saveOrderActivity(orderRecord: SolanaAuctionHouseOrderRecord) {
+        val activityDto = orderActivityConverter.convert(orderRecord, false)
+        if (activityDto != null) {
+            activityRepository.save(activityDto)
+        }
     }
 
     companion object {
