68ce95cc04847e7cccf8cf8cb70a8afe51f13fe3
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-04-13 12:18:36 +0300

    feat: implement signature validation.

--- solana-api/build.gradle.kts
+++ solana-api/build.gradle.kts
@@ -4,6 +4,9 @@ val solanaOpenapiVersion: String by project
 
 dependencies {
     implementation(project(":solana-common"))
+    implementation("com.google.crypto.tink:tink:1.6.1")
+    implementation("org.bitcoinj:bitcoinj-core:0.16.1")
+
     testImplementation("com.rarible.protocol.solana:solana-protocol-api:$solanaOpenapiVersion")
     testImplementation(project(":solana-test-common"))
     testImplementation("com.rarible.protocol.solana:solana-protocol-client:$solanaOpenapiVersion")
--- solana-api/src/main/kotlin/com/rarible/protocol/solana/nft/api/controller/SignatureController.kt
+++ solana-api/src/main/kotlin/com/rarible/protocol/solana/nft/api/controller/SignatureController.kt
@@ -1,17 +1,48 @@
 package com.rarible.protocol.solana.nft.api.controller
 
+import com.google.crypto.tink.subtle.Ed25519Verify
+import com.google.crypto.tink.subtle.Hex
 import com.rarible.protocol.solana.api.controller.SignatureControllerApi
 import com.rarible.protocol.solana.dto.SolanaSignatureValidationFormDto
+import com.rarible.protocol.solana.nft.api.exceptions.ValidationApiException
+import org.bitcoinj.core.Base58
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SignatureController : SignatureControllerApi {
 
+    private val logger = LoggerFactory.getLogger(SignatureController::class.java)
+
     override suspend fun validate(
         solanaSignatureValidationFormDto: SolanaSignatureValidationFormDto
     ): ResponseEntity<Boolean> {
-        // TODO IMPLEMENT
-        return ResponseEntity.ok(false)
+        val signature = solanaSignatureValidationFormDto.signature
+        val signatureBytes = try {
+            Hex.decode(signature)
+        } catch (e: Exception) {
+            throw ValidationApiException("Invalid signature, must be 128 hex string, but was $signature")
+        }
+
+        val signer = solanaSignatureValidationFormDto.signer
+        if (signer.isEmpty()) {
+            throw ValidationApiException("Signer is empty")
+        }
+        val signerBytes = try {
+            Base58.decode(signer)
+        } catch (e: Exception) {
+            throw ValidationApiException("Signer must be in base58, but was $signer")
+        }
+
+        val message = solanaSignatureValidationFormDto.message
+        try {
+            Ed25519Verify(signerBytes).verify(signatureBytes, message.toByteArray())
+        } catch (e: Exception) {
+            logger.info("Wrong signature '$signature' by signer '$signer' for data '$message'")
+            return ResponseEntity.ok(false)
+        }
+
+        return ResponseEntity.ok(true)
     }
 }
