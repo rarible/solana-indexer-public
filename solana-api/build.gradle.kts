2a74cdf6ee1305d8111cce7bcaf1205b5c5c8f5b
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-05-28 22:16:46 +0700

    feat: add APM starter dependency.

--- solana-api/build.gradle.kts
+++ solana-api/build.gradle.kts
@@ -1,9 +1,11 @@
 apply(plugin = "server")
 
 val solanaOpenapiVersion: String by project
+val raribleCommonVersion: String by project
 
 dependencies {
     implementation(project(":solana-common"))
+    implementation("com.rarible.core:rarible-core-apm-starter:$raribleCommonVersion")
 
     testImplementation("com.rarible.protocol.solana:solana-protocol-api:$solanaOpenapiVersion")
     testImplementation(project(":solana-test-common"))
