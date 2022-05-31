094989cfc48f94f511485f3cdc8401f298131e27
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-05-31 17:47:00 +0700

    fix: update makeStock of sell orders owner of the account(balance) changes.

--- solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/service/balance/BalanceUpdateService.kt
+++ solana-indexer/src/main/kotlin/com/rarible/protocol/solana/nft/listener/service/balance/BalanceUpdateService.kt
@@ -1,7 +1,6 @@
 package com.rarible.protocol.solana.nft.listener.service.balance
 
 import com.rarible.core.entity.reducer.service.EntityService
-import com.rarible.protocol.solana.common.filter.token.SolanaTokenFilter
 import com.rarible.protocol.solana.common.model.Balance
 import com.rarible.protocol.solana.common.model.BalanceId
 import com.rarible.protocol.solana.common.model.isEmpty
@@ -10,7 +9,6 @@ import com.rarible.protocol.solana.common.update.BalanceUpdateListener
 import com.rarible.protocol.solana.nft.listener.service.order.OrderMakeStockBalanceUpdateService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.math.BigInteger
 
 @Component
 class BalanceUpdateService(
@@ -29,19 +27,10 @@ class BalanceUpdateService(
             logger.info("Balance without Initialize record, skipping it: {}", entity)
             return entity
         }
-        val exist = balanceRepository.findByAccount(entity.account)
         val balance = balanceRepository.save(entity)
-        if (exist != null) {
-            balanceUpdateListener.onBalanceChanged(balance)
-        }
-        if (exist != null && entity.value == BigInteger.ZERO) {
-            logger.info("Deleted balance: $entity")
-        } else {
-            logger.info("Updated balance: $entity")
-        }
-        if (exist?.value != balance.value) {
-            orderMakeStockBalanceUpdateService.updateMakeStockOfSellOrders(balance)
-        }
+        balanceUpdateListener.onBalanceChanged(balance)
+        orderMakeStockBalanceUpdateService.updateMakeStockOfSellOrders(balance)
+        logger.info("Updated balance: $balance")
         return balance
     }
 
