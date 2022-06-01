bdcb6f3e3361486e548f850d9ebc426925ddbe3b
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-06-01 08:23:34 +0700

    cleanup: unused migration.

--- solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002MarkOutcomeActivities.kt
+++ solana-migration/src/main/kotlin/com/rarible/protocol/solana/nft/migration/mongock/ChangeLog00002MarkOutcomeActivities.kt
@@ -4,7 +4,6 @@ import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
 import com.rarible.protocol.solana.common.model.ActivityRecord
 import com.rarible.protocol.solana.common.repository.ActivityRepository
-import com.rarible.protocol.solana.dto.ActivityTypeDto
 import io.changock.migration.api.annotations.NonLockGuarded
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.runBlocking
@@ -13,44 +12,11 @@ import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.data.mongodb.core.aggregation.AggregationUpdate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
-import org.springframework.data.mongodb.core.query.Update
 
 @ChangeLog(order = "00002")
 class ChangeLog00002MarkOutcomeActivities {
 
     private val logger = LoggerFactory.getLogger(ChangeLog00002MarkOutcomeActivities::class.java)
-
-    @ChangeSet(
-        id = "ChangeLog00002MarkOutcomeActivities.markOutcomeActivities",
-        order = "00002",
-        author = "protocol"
-    )
-    fun markOutcomeActivities(
-        @NonLockGuarded activityRepository: ActivityRepository,
-        @NonLockGuarded mongo: ReactiveMongoOperations
-    ) = runBlocking {
-        var processed = 0
-        activityRepository.findAllActivities(
-            types = listOf(ActivityTypeDto.TRANSFER),
-            continuation = null,
-            size = Int.MAX_VALUE,
-            sortAscending = true
-        ).collect { activity ->
-            if (activity.id.endsWith("outcome")) {
-
-                val criteria = Criteria.where("_id").`is`(activity.id)
-
-                mongo.updateFirst(
-                    Query(criteria),
-                    Update().set("outcome", "true"),
-                    ActivityRepository.COLLECTION
-                ).awaitFirst()
-            }
-            if (++processed % 10000 == 0) {
-                logger.info("Processed $processed activities")
-            }
-        }
-    }
 
     @ChangeSet(
         id = "ChangeLog00002MarkOutcomeActivities.updateActivityDbUpdateField",
