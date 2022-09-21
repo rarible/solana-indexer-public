8fbbbc860521e7669f5bf67652212d6f142811d6
Artem Yakischik <a.yakischik@rarible.com>
2022-09-21 14:21:26 +0400

    feat: Client tag contributor for API requests

--- solana-api/build.gradle.kts
+++ solana-api/build.gradle.kts
@@ -6,6 +6,7 @@ val raribleCommonVersion: String by project
 dependencies {
     implementation(project(":solana-common"))
     implementation("com.rarible.core:rarible-core-apm-starter:$raribleCommonVersion")
+    implementation("com.rarible.core:rarible-core-telemetry-starter:$raribleCommonVersion")
 
     testImplementation("com.rarible.protocol.solana:solana-protocol-api:$solanaOpenapiVersion")
     testImplementation(project(":solana-test-common"))
--- solana-api/src/main/kotlin/com/rarible/protocol/solana/nft/api/configuration/NftIndexerApiConfiguration.kt
+++ solana-api/src/main/kotlin/com/rarible/protocol/solana/nft/api/configuration/NftIndexerApiConfiguration.kt
@@ -1,10 +1,19 @@
 package com.rarible.protocol.solana.nft.api.configuration
 
+import com.rarible.core.telemetry.actuator.WebRequestClientTagContributor
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(NftIndexerApiProperties::class)
 class NftIndexerApiConfiguration(
     private val nftIndexerApiProperties: NftIndexerApiProperties
-)
+) {
+
+    @Bean
+    fun webRequestClientTagContributor(): WebRequestClientTagContributor {
+        return WebRequestClientTagContributor()
+    }
+
+}
